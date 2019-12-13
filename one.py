8ec9d503d6bc421a4886850a560cfc95223d075d
Raj2Herald <raj.shrestha@heraldcollege.edu.np>
2019-12-13 21:22:50 +0545

    dfgasfgasdf

--- one.py
+++ one.py
@@ -1,5 +1,5 @@
 print("Hello.... hello hi hi hi hi ")
 print("Hello....Hello Hello Hello 123")
 print("asdsadasdasdasdasdas")
-print("sadasdasfsdafhello ")
+print("sadasdasfsdafhello hi hi hi 123")
 
