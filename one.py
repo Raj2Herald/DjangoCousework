a67da9d8b1958bc4c1fb9d35e594792a8766e249
Raj2Herald <raj.shrestha@heraldcollege.edu.np>
2019-12-13 21:22:22 +0545

    New changes

--- one.py
+++ one.py
@@ -1,5 +1,5 @@
 print("Hello.... hello hi hi hi hi ")
 print("Hello....Hello Hello Hello 123")
 print("asdsadasdasdasdasdas")
-print("sadasdasfsdafhello ")
+print("sadasdasfsdafhello hello 123 123 ")
 
